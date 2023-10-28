cf7d208d9c6b57c8aa441c3823275dec00161a08
Olga Em <olga.em020@gmail.com>
2023-10-29 02:45:05 +0700

    Add validation test

--- src/test/java/ru/netology/web/test/WebInterfaceTest.java
+++ src/test/java/ru/netology/web/test/WebInterfaceTest.java
@@ -50,4 +50,16 @@ public class WebInterfaceTest {
         assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
         Thread.sleep(5000);
     }
+
+    @Test
+    void shouldTestValidName() {
+        List<WebElement> elements = driver.findElements(By.className("input__control"));
+        elements.get(0).sendKeys("Эм Ольга+");
+        elements.get(1).sendKeys("+79140000000");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button")).click();
+        String text = driver.findElement(By.className("input__sub")).getText();
+        assertEquals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.", text.trim());
+
+    }
 }
